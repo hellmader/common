c4b04c99b2f1b54393d9c443e0bbe547666f3777
hellmader <103506678+hellmader@users.noreply.github.com>
2023-08-22 17:23:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,15 +9,10 @@ do
     esac
 done
 
-echo "#####################################"
-
 Akkuname="$Seriennummer"
-echo "Token: $Token"
-echo "Akkuname: $Akkuname"
-echo "Seriennummer: $Seriennummer"
-echo "GitToken: $gitToken"
-
-echo "#####################################"
-
+#echo "Token: $Token"
+#echo "Akkuname: $Akkuname"
+#echo "Seriennummer: $Seriennummer"
+#echo "GitToken: $gitToken"
 
 curl -s "https://${gitToken}@raw.githubusercontent.com/hellmader/ProfiLink/master/install/install.sh" | bash -s -- -t ${Token}  -s ${Seriennummer}  -g ${gitToken}
